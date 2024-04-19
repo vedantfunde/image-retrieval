981343c113c63a093a7a42144f2f2cd7303bb115
Vedant Funde <120516171+vedantfunde@users.noreply.github.com>
2024-04-20 03:06:51 +0530

    Update and rename naive bayes to naive bayes.py

--- classifiers/naive bayes.py
+++ classifiers/naive bayes.py
@@ -1,14 +1,14 @@
 from sklearn.naive_bayes import GaussianNB
 
-# Initialize Gaussian Naive Bayes classifier
+#initialize classifier
 naive_bayes_classifier = GaussianNB()
 
-# Fit the classifier to the training data
+#fitting the classifier
 naive_bayes_classifier.fit(features_pca, labels)
 
-# Make predictions on the test data
+#make predictions
 predicted_labels = naive_bayes_classifier.predict(test_features_pca)
 
-# Calculate accuracy
+#calculating accuracy
 accuracy = np.mean(predicted_labels == test_labels)
 print("Accuracy:", accuracy)
