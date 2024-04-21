44f0ae27c072935af4d8bcb53ed8464ce4c2c2dd
AvadhutJadhav96 <128384459+AvadhutJadhav96@users.noreply.github.com>
2024-04-21 14:01:56 +0530

    Update random forest.py

--- classifiers/random forest.py
+++ classifiers/random forest.py
@@ -5,9 +5,8 @@ random_forest_classifier = RandomForestClassifier(n_estimators=100, random_state
 
 kf = KFold(n_splits=num_folds, shuffle=True, random_state=42)
 
-# Perform cross-validation
+# Cross Validation to aovid overfitting
 cv_scores = cross_val_score(random_forest_classifier, features_pca, labels, cv=kf)
 
-# Display the cross-validation scores
 print("Cross-validation scores:", cv_scores)
 print("Mean cross-validation score:", np.mean(cv_scores))
