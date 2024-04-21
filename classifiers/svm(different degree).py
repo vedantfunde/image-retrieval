6c8f49251083436847e5fdaac2123fd64f1cd0d8
b22237 <120501753+b22237@users.noreply.github.com>
2024-04-21 15:19:50 +0530

    Update svm(different degree).py

--- classifiers/svm(different degree).py
+++ classifiers/svm(different degree).py
@@ -1,27 +1,18 @@
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score, KFold
 
-#consider any degrees of polynomial
-param_grid = {'degree': [2, 3, 4, 5]} 
-
-#initializing polynomial kernel
+param_grid = {'degree': [2, 3, 4, 5]}
 svm_classifier_poly = SVC(kernel='poly')
-
-#initializing GridSearchCV with SVM classifier and parameter grid
 grid_search = GridSearchCV(svm_classifier_poly, param_grid, cv=5, scoring='accuracy')
-
-#fitting GridSearchCV to the training data
-grid_search.fit(features_pca, labels)
-
-#getting the best degree from the grid search results
+grid_search.fit(features_lda, labels)
 best_degree = grid_search.best_params_['degree']
 print("Best degree:", best_degree)
-
-#getting the best SVM classifier with the best degree
 best_svm_classifier = grid_search.best_estimator_
-
-#making predictions on the test data using the best classifier
-predicted_labels = best_svm_classifier.predict(test_features_pca)
-
-#calculating accuracy
+num_folds = 5
+kf = KFold(n_splits=num_folds, shuffle=True, random_state=42)
+cv_scores = cross_val_score(best_svm_classifier, features_lda, labels, cv=kf)
+print("Cross-validation scores:", cv_scores)
+print("Mean cross-validation score:", np.mean(cv_scores))
+predicted_labels = best_svm_classifier.predict(test_features_lda)
 accuracy = np.mean(predicted_labels == test_labels)
-#print("Accuracy:", accuracy)
+print("Accuracy:", accuracy)
