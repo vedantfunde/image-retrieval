f76769c8cb7e917f57495cd2d7f20319d14260ad
AvadhutJadhav96 <128384459+AvadhutJadhav96@users.noreply.github.com>
2024-04-21 13:59:23 +0530

    Update knn.py

--- classifiers/knn.py
+++ classifiers/knn.py
@@ -1,14 +1,13 @@
 from sklearn.neighbors import KNeighborsClassifier
 
-#loading the trained KNN classifier
+#loading the trained KNN classifier, using library function
 knn_classifier = KNeighborsClassifier()
 
-#fitting the KNN classifier to the training data
+
 # knn_classifier.fit(features_lda, labels)
 knn_classifier.fit(features_pca, labels)
 #making predictions
 predicted_labels = knn_classifier.predict(test_features_pca)
 
-#calculating accuracy
 accuracy = np.mean(predicted_labels == test_labels)
 #print("Accuracy:", accuracy)
