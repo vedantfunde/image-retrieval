e1ee0536c7350f15d6ae3d024fef1b4866bd22e7
b22237 <120501753+b22237@users.noreply.github.com>
2024-04-21 13:52:59 +0530

    Update Image Retrieval.py

--- Image Retrieval.py
+++ Image Retrieval.py
@@ -1,38 +1,30 @@
 import numpy as np
-import torch
-import torchvision.models as models
+
 import torchvision.transforms as transforms
 from PIL import Image
 from sklearn.neighbors import NearestNeighbors
 import matplotlib.pyplot as plt
-from sklearn.svm import SVC
-import torch.nn as nn
+import os  # Import os module for file operations
 import requests
-from io import BytesIO  # Import BytesIO module
-import gdown
+
+import torch
+import torchvision.transforms as transforms
+import torchvision.models as models
+
 import pickle
-
-url_features_lda = 'https://drive.google.com/uc?id=15vdHeT0xvQcBkPOvF-uae4_dGtqsuK2s'
-gdown.download(url_features_lda, 'features_lda.npy', quiet=False)
-
 features_lda = np.load('features_lda.npy')
-
-url_labels = 'https://drive.google.com/uc?id=1wbW1ZKNyT9bLsaIi1uOmD9HQ4OZeqDqm'
-gdown.download(url_labels, 'labels.npy', quiet=False)
-
 labels = np.load('labels.npy')
-
-url_stored_data = 'https://drive.google.com/uc?id=1pim6rKyVM8K_nGPNtPkI3dwh7_zcSzJY'
-gdown.download(url_stored_data, 'stored_data.pkl', quiet=False)
-
 with open('stored_data.pkl', 'rb') as f:
     stored_data = pickle.load(f)
-features_lda.shape
-
-
-svm_classifier = SVC(kernel='rbf')
-
-svm_classifier.fit(features_lda, labels)
+with open('lda.pkl', 'rb') as f:
+    lda = pickle.load(f)
+with open('svm_classifier.pkl', 'rb') as f:
+    svm_classifier = pickle.load(f)
+resnet = models.resnet50(pretrained=True)
+# Remove the last fully connected layer
+resnet = nn.Sequential(*list(resnet.children())[:-1])
+# Set the model to evaluation mode
+resnet.eval()
 
 def extract_features_from_image(input_image, model, lda):
     preprocess = transforms.Compose([
@@ -49,36 +41,55 @@ def extract_features_from_image(input_image, model, lda):
     features = lda.transform(features.unsqueeze(0))
     return features[0]
 
-def retrieve_similar_images_from_input_image(input_image_features, k=5):
+def save_similar_images(similar_image_indices, stored_data, output_dir):
+    similar_image_paths = []
+    for i, index in enumerate(similar_image_indices):
+        image_data = np.stack((stored_data[index][b'R'], stored_data[index][b'G'], stored_data[index][b'B']), axis=-1)
+        similar_image = Image.fromarray(image_data)
+        # Define the path to save the image
+        image_path = os.path.join(output_dir, f'similar_image_{i + 1}.jpg')
+        # Save the image
+        similar_image.save(image_path)
+        similar_image_paths.append(image_path)
+    return similar_image_paths
+
+def retrieve_similar_images_from_input_image(input_image_features, k=5, output_dir='similar_images'):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
     predicted_label = svm_classifier.predict([input_image_features])[0]
     similar_indices = [i for i, label in enumerate(labels) if label == predicted_label]
     nn_model = NearestNeighbors(n_neighbors=k)
     nn_model.fit(features_lda[similar_indices])
     distances, indices = nn_model.kneighbors([input_image_features])
     original_indices = [similar_indices[i] for i in indices[0]]
-    return original_indices
+    
+    # Save similar images locally and return their paths
+    similar_image_paths = save_similar_images(original_indices, stored_data, output_dir)
+    return similar_image_paths
 
-def visualize_retrieved_images(input_image, similar_image_indices):
+def visualize_retrieved_images(input_image, similar_image_paths):
     plt.figure(figsize=(15, 5))
     plt.subplot(1, 6, 1)
     plt.imshow(input_image)
     plt.title('Input Image')
     plt.axis('off')
 
-    for i, index in enumerate(similar_image_indices):
+    for i, image_path in enumerate(similar_image_paths):
         plt.subplot(1, 6, i + 2)
-        image_data = np.stack((stored_data[index][b'R'], stored_data[index][b'G'], stored_data[index][b'B']), axis=-1)
-        plt.imshow(Image.fromarray(image_data))
+        image_data = np.array(Image.open(image_path))
+        plt.imshow(image_data)
         plt.title(f'Similar Image {i + 1}')
         plt.axis('off')
 
     plt.show()
 
-def image_retrieval(input_image_path):
+def image_retrieval(input_image_path, resnet, lda):
     input_image = Image.open(input_image_path)
     input_image_features = extract_features_from_image(input_image, resnet, lda)
-    similar_image_indices = retrieve_similar_images_from_input_image(input_image_features)
-    visualize_retrieved_images(input_image, similar_image_indices)
-
-# input_image_path = '/content/drive/MyDrive/ml/project/dog-puppy-on-garden-royalty-free-image-1586966191.jpg'
-# image_retrieval(input_image_path)
+    similar_image_paths = retrieve_similar_images_from_input_image(input_image_features)
+    visualize_retrieved_images(input_image, similar_image_paths)
+    return(similar_image_paths)
+# input_image_path = '/content/download (8).jpeg'
+# similar_image_paths=image_retrieval(input_image_path, resnet, lda)
+# print(similar_image_paths)
